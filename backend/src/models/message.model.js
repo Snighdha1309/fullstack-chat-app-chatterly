ae54a858d340ed6107aab0adc16d8469d8de3ac0
Snighdha <snighdha1303@gmail.com>
2025-06-09 03:23:30 +0530

    .

--- backend/src/models/message.model.js
+++ backend/src/models/message.model.js
@@ -1,3 +1,5 @@
+import mongoose from "mongoose";
+
 const messageSchema = new mongoose.Schema(
   {
     senderId: {
@@ -16,14 +18,10 @@ const messageSchema = new mongoose.Schema(
     image: {
       type: String,
     },
-    audio: {
-      type: String, // optional if you allow voice notes
-    },
-    video: {
-      type: String, // <-- Add this line for video URL
-    },
   },
   { timestamps: true }
 );
-const  Message = mongoose.model("Message",messageSchema);
+
+const Message = mongoose.model("Message", messageSchema);
+
 export default Message;
