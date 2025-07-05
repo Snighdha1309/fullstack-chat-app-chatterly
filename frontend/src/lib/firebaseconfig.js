d557bfb64c8907a80e75e7aa40beb7aa8ab0138f
Snighdha <snighdha1303@gmail.com>
2025-07-05 13:03:25 +0530

    ..

--- frontend/src/lib/firebaseconfig.js
+++ frontend/src/lib/firebaseconfig.js
@@ -7,13 +7,13 @@ import { getAuth } from "firebase/auth";
 let analytics;
 
 const firebaseConfig = {
-   apiKey: "AIzaSyDOs9fCDArxLlpVHNQ89KgOecEd5Z-H6Ds",
-  authDomain: "emailauthchatterly.firebaseapp.com",
-  projectId: "emailauthchatterly",
-  storageBucket: "emailauthchatterly.firebasestorage.app",
-  messagingSenderId: "871321271681",
-  appId: "1:871321271681:web:1c914cc54561023dd4bc80",
-  measurementId: "G-M0XWJT2Z5L"
+   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
